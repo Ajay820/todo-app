e3bc4cde751d87b498032e57c67997204b604b70
Ajay <Ajay4uu.ak@gmail.com>
2023-03-12 08:21:15 +0530

    proper implemenatation with all error handling

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,74 +3,110 @@ const mongoose = require("mongoose")
 const Todo = require("../model/todo.js")
 
 exports.createTodo = async (req,res)=>{
-    const {title,tasks} = req.body
+    try{
+        const {title,tasks} = req.body
     
-    if(!title){
-        res.status(401).send("please provide title and that is mandatory")
+        if(!title){
+            res.status(401).send("please provide title and that is mandatory")
+        }
+        else{
+            const user = await Todo.create({
+                title:title,
+                tasks:tasks
+            })
+    
+            res.status(200).json({user})
+    
+        }
     }
-    else{
-        const user = await Todo.create({
-            title:title,
-            tasks:tasks
+    catch(error){
+        res.staus(401).json({
+            success:false,
+            message:"unable to create a todo"
         })
-
-        res.status(200).json({user})
-
     }
-
 }
 
 exports.createTask = async (req,res)=>{
-    const {id} = req.params
+    try{
 
-    const {tasks} = req.body
+        const {id} = req.params
 
-
-    const user = await Todo.findById(id)
-
-    user.tasks.push(tasks)
-
-    user.save()
-
-    res.status(200).json({
-        success:true,
-        user
-    })
-
+        const {tasks} = req.body
+    
+        const user = await Todo.findById(id)
+    
+        user.tasks.push(tasks)
+    
+        user.save()
+    
+        res.status(200).json({
+            success:true,
+            user
+        })
+    }
+    catch(error){
+        res.status(401),json({
+            success:false,
+            message:"unable to add tasks"
+        })
+    }
 }
 
 exports.getAllTodos=async (req,res)=>{
+    try{
+        const user = await Todo.find()
 
-const user = await Todo.find()
+        const todos = user
 
-const todos = user
+        const data = todos.map((todo)=>(todo.title))
 
-const data = todos.map((todo)=>(todo.title))
+        res.status(200).send(data)
+    }
 
-res.status(200).send(data)
+    catch(error){
+        res.status(401).json({
+            success:true,
+            message:"No todo Exists"
+        })
 
+}
 }
 
 exports.deleteTasks = async (req,res)=>{
 
-    const {id} = req.params
+    try{
 
-    const {tasks} = req.body
+        const {id} = req.params
 
-    const user = await Todo.findById(id)
+        const {tasks} = req.body
 
-    const ans = user.tasks.indexOf(tasks)
+        const user = await Todo.findById(id)
 
-    console.log(ans)
+        const ans = user.tasks.indexOf(tasks)
 
-    await user.tasks.splice(ans,1)
-    
-    user.save()
+        if(ans != -1){
+            await user.tasks.splice(ans,1)
+            user.save()
+            res.status(200).json({
+                success:true,
+                user
+            })
+        }
 
-    res.status(200).json({
-        success:true,
-        user
-    })
+        else{
+            res.status(200).json({
+                success:false,
+                message:"unable to find todo"
+            })
+        }
+    }
+    catch(error){
+        res.status(401).json({
+            success:false,
+            message:"unable to find the task"
+        })
+    }
 }
 
 exports.updateTasks=async (req,res)=>{
@@ -91,14 +127,24 @@ exports.updateTasks=async (req,res)=>{
 }
 
 exports.deleteTodo =async (req,res)=>{
-    const {id} = req.params
 
-    const user = await Todo.findByIdAndDelete(id)
+    try{
+        const {id} = req.params
 
-    res.status(200).json({
-        success:true,
-        message:"todo delete successfully"
-    })
+        const user = await Todo.findByIdAndDelete(id)
+    
+        res.status(200).json({
+            success:true,
+            message:"todo delete successfully"
+        })
+    }
+
+    catch(error){
+        res.status(401).json({
+            success:false,
+            message:"unable to find the todo"
+        })
+    }
 }
 
 
